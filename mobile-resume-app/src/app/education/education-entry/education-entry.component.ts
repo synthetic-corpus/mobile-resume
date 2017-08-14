d8e96f3d4eac739c9f649b33f3198309163ef7ba
Syn <joel.gonzaga@gmail.com>
2017-08-14 11:27:34 -0700

    tested alternating background colors for education entries and it works as expected.

--- mobile-resume-app/src/app/education/education-entry/education-entry.component.ts
+++ mobile-resume-app/src/app/education/education-entry/education-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-education-entry',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationEntryComponent implements OnInit {
 
+  style: string;
+  @Input() entry: {name:string, webaddy:string, icon:string, graduation:string, summary:string};
+  @Input() index: number;
+
   constructor() { }
 
   ngOnInit() {
+    // used to alternate background colors.
+    if (this.index % 2 === 0){
+      this.style = "row light-tan";
+    }
+    else{
+      this.style = "row tan";
+    }
   }
 
 }
