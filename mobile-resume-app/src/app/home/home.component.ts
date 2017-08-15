027cbcfd57a16c86ca30c8bb733fcaaa214cc8a6
Syn <joel.gonzaga@gmail.com>
2017-08-15 10:10:27 -0700

    have the header displaying as I like it. Only needs a change in fonts and background image now.

--- mobile-resume-app/src/app/home/home.component.ts
+++ mobile-resume-app/src/app/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+/* Controls the header image with potrait, background etc at the top of the page */
+
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { HttpcallService } from '../services/httpCall.service';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  skillarray: string[];
 
-  constructor() { }
+  constructor(
+    private httpcallService: HttpcallService
+  ) { }
 
   ngOnInit() {
+    this.httpcallService.getSkills()
+      .subscribe(
+        (response: Response) => {
+          const skills = response.json();
+          this.skillarray = skills;
+        }
+    );
+
   }
+
 
 }
