44bb8842ee5026725b60a4fc4416e60a4d6578c8
Syn <joel.gonzaga@gmail.com>
2017-08-15 12:59:47 -0700

    there isn't a good module solution. Must be built with raw javascript functions in app.component.ts have begun writing three functions to do just this.

--- mobile-resume-app/src/app/app.component.ts
+++ mobile-resume-app/src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  /* This is a for scrolling */
+  scrollToElement(elementY){
+    // Get the current location of this browser
+    var locationY: number = window.scrollY;
+    // Set the speed and increment of how fast it scrolls.
+    const scrollSpeed = 1;
+    const scrollDelta = 5;
+    // Loop through a continues scroll up until locationY == elementY
+
+  }
+
+  getElementY(id){
+    // must write a function to get the Y of an element by its ID
+  }
+
+  smoothScroll(id) {
+    // combines the above to functions to achieve a smooth scroll.
+    const elementY = this.getElementY(id);
+    this.scrollToElement(elementY);
+  }
 }
